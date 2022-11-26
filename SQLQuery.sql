2f0e5c95871fb5819b343996338bb6cc683861e4
huynguyendev02 <109943707+huynguyendev02@users.noreply.github.com>
2022-11-26 09:50:03 +0700

    Update SQLQuery.sql

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE DatabaseEC
+    CREATE DATABASE DatabaseEC
 GO
 
 USE DatabaseEC
@@ -119,7 +119,7 @@ GO
 
 CREATE TABLE order_details (
 	id					int	IDENTITY(1,1) primary key,
-	users_id			int		    	foreign key references users(id),
+	users_id			int			foreign key references users(id),
 	total				decimal,
 	payment_id			int			foreign key references payment_details(id),
 	created_at			date,
@@ -138,29 +138,38 @@ CREATE TABLE order_items (
 GO
 
 insert into product_category (name_category, desc_category)
-values 
-	('Laptop','Máy tính di động'),
-	('Điện thoại', 'Điện thoại di động'),
-	('Máy tính bảng', 'Thiết bị di động cỡ lớn'),
-	('Máy lạnh', 'Thiết bị điều hoà'),
-	('Bộ sạc', 'Nạp lại năng lượng')
+values
+	('Mac','Which Mac is right for you?'),
+	('iPad', 'Introducing the all‑new iPad and iPad Pro.'),
+	('iPhone', 'Which iPhone is right for you?'),
+	('Watch', 'Any case. Any band. Any style you want.'),
+	('Airpods', 'A magical connection to your devices.'),
+    ('TV & Home', 'The future hits home.'),
+    ('Accessories','Find the accessories you’re looking for.')
 
 insert into product_inventory (quantity)
 values
-	(123),(100),(54),(454),(2333),(999)
-
-insert into discount (name_discount,desc_discount,discount_percent,active)
-values 
-	('Đại hạ giá valentine','Hạ giá toàn bộ khi các cặp đôi đến mua vào ngày 14/2',0.12, 0),
-	('Giáng sinh rực rỡ', 'Đại hạ giá noel dành cho các thiết bị laptop trong ngày 24-26/12',0.20,0)
+	(1234),(1212),(3232),(2121),(1717),(1414)
 
 insert into product (name_product,desc_product,category_id,inventory_id,price,discount_id,image_url)
-values 
-	('Apple Iphone 14 Pro Max','Dòng Iphone mới nhất với dynamic island đến từ nhà Apple', 2, 1, 1299, null,null),
-	('Apple Macbook Pro 16inch', 'Laptop dành cho những người làm đồ hoạ, IT', 1,2,2999,null,null),
-	('Samsung Galaxy Tab S8 Ultra', 'Dòng tablet mới nhất, xịn nhất từ nhà Samsung', 3,3,1999,null,null),
-	('Máy lạnh Panasonic Inverter 1 HP CU/CS-XU9XKH-8', 'Máy lạnh Panasonic Inverter 1 HP CU/CS-XU9XKH-8 thiết kế màu trắng với phần vỏ làm bằng nhựa phủ một lớp nhám. Trên máy không có màn hình hiển thị nhiệt độ.',4,4,999,null,null),
-	('Củ sạc Xiaomi Mi Charger 20W Type-C', 'Bộ sạc nhanh Xiaomi Mi 20W Charger mà bạn đang theo dõi là giải pháp tối ưu để sạc nhanh cho các dòng smartphone thế hệ mới, sản phẩm có đầu ra 20W, cổng USB Type-C và đảm bảo an toàn cho người dùng nhờ cơ chế điều tiết dòng điện thông minh.', 5,5,12,null,null)
+values
+	('MacBook Air','Don’t take it lightly.', 1, 1, 9499, null,'https://iili.io/H9zWSFS.png'),
+	('MacBook Pro 13”', 'Pro anywhere.', 1,1,9999,null,'https://iili.io/H9zWZPV.png'),
+	('MacBook Pro 14” and 16”', 'Incredible power. Incredibly simple.', 1,1,15999,null,'https://iili.io/H9zXoRn.png'),
+	('iPad', 'Lovable. Drawable. Magical.',2,2,3499,null,'https://iili.io/H9zXDXf.png'),
+	('iPad Pro', 'Supercharged by M2', 2,2,6499,null,'https://iili.io/H9zh3mu.png'),
+    ('iPad mini', 'The full iPad experience designed to fit in one hand.',2,2,3999,null,'https://iili.io/H9zjBun.png'),
+    ('iPhone 14','Big and bigger.',3,3,9499,null,'https://iili.io/H9zjxnf.png'),
+    ('iPhone 14 Pro', 'Pro. Beyond.',3,3,12999,null,'https://iili.io/H9zjzG4.png'),
+    ('iPhone SE','Love the power. Love the price.',3,3,5999,null,'https://iili.io/H9zjAaS.png'),
+    ('Apple Watch Ultra', 'Adventure awaits.', 4,4,6399,null,'https://iili.io/H9zjYue.png'),
+    ('Apple Watch Series','A healthy leap ahead.',4,4,3199,null,'https://iili.io/H9zjE6Q.png'),
+    ('Apple Watch SE', 'A great deal to love.',4,4,1999,null,'https://iili.io/H9zjgSI.png'),
+    ('AirPods Pro', '2nd generation', 5,5,1849,null,'https://iili.io/H9zjDKl.png'),
+    ('AirPods Max','Personalized Spatial Audio with dynamic head tracking',5,5,4599,null,'https://freeimage.host/i/H9zwUsj'),
+    ('Apple Pencil (2nd generation)','Designed for iPad Pro and iPad Air',6,6,999,null,'https://iili.io/H9zwtzg.png'),
+    ('MagSafe Charger','Mix. Match. MagSafe.',6,6,299,null,'https://iili.io/H9zwpqv.png')
+
 
 insert into users(username,passwords,first_name,last_name,telephone)
 values
@@ -183,28 +192,7 @@ insert into user_payment (
 	providers,
 	account_no,
 	expiry)
-values 
+values
 	(2,'Credit Card','VISA',1,'2026-11-11')
-go
-UPDATE product
-SET image_url = 'https://cdn.tgdd.vn/Products/Images/42/289702/iphone-14-pro-max-bac-thumb-600x600.jpg'
-WHERE id=1;
-go
-
-UPDATE product
-SET image_url = 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp16-spacegray-select-202110?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1632788574000'
-WHERE id=2;
-go
-UPDATE product
-SET image_url = 'https://images.samsung.com/is/image/samsung/assets/vn/galaxy-tab-s8/feature/galaxy-tab-s8-ultra-graphite-kv-mo.jpg'
-WHERE id=3;
-go
-UPDATE product
-SET image_url = 'https://cdn01.dienmaycholon.vn/filewebdmclnew/DMCL21/Picture//Apro/Apro_product_24399/may-lanh-panaso_multi_0_49_450.png.webp'
-WHERE id=4;
-go
-UPDATE product
-SET image_url = 'https://cdn.tgdd.vn/Files/2020/11/03/1304338/xiaomi-ra-mat-cu-sac-20w-tuong-thich-voi-iphone-12-2.jpg'
-WHERE id=5;
 
 
